47fd534218246051468e1e76b044b60281b262d2
TVasilj <50152563+TVasilj@users.noreply.github.com>
2019-05-05 20:38:08 +0200

    Code refactoring

--- Motion-Calculator.ino
+++ Motion-Calculator.ino
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <Wire.h>                              // Wire library - used for I2C communication
 #include <LiquidCrystal_I2C.h>
 
@@ -13,8 +12,10 @@ char row3[10];
 uint8_t currentRow = 1;
 int32_t result;
 
+void sendDataToESP();
 LiquidCrystal_I2C lcd(0x27, 20, 4);            // Set the LCD address to 0x27 for a 20 chars and 4 line display
 void lcdPrint(uint8_t incomingByte);
+void rowFill(uint8_t row, char* ch);
 
 void setup() {
   Serial.begin(115200);                        // Initiate serial communication for printing the results on the Serial monitor
@@ -33,126 +34,64 @@ void setup() {
   Serial.println(0);
 }
 void loop() {
+  if (millis() - time > 100000) {
+    time = millis();
+    sendDataToESP();                           // function used to send data to ESP app
+  }
+  
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     if (incomingByte >= 48 && incomingByte <= 57) {
       lcdPrint(incomingByte);  
     }
   } 
- 
-  if (millis() - time > 10000) {
-    time = millis();
-    // === Read acceleromter data === //
-    Wire.beginTransmission(ADXL345);
-    Wire.write(0x32);                            // Start with register 0x32 (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(ADXL345, REG_SIZE, true);   // Read 6 registers total, each axis value is stored in 2 registers
-    X = ( Wire.read()| Wire.read() << 8);        // X-axis value
-    X = X/256;                                   //For a range of +-2g, we need to divide the raw values by 256, according to the datasheet
-    Y = ( Wire.read()| Wire.read() << 8);        // Y-axis value
-    Y = Y/256;
-    Z = ( Wire.read()| Wire.read() << 8);        // Z-axis value
-    Z = Z/256;
-    
-    Serial.print(X);
-    Serial.print("\t");
-    Serial.print(Y);
-    Serial.print("\t");
-    Serial.println(Z);
-    
-    lcd.clear();                                 // clear previous values from the screen
-    lcd.setCursor(0,0);                          // sets cursor to column: 0 , row: 0
-    lcd.print("X:");                             // print X: to the screen
-    lcd.setCursor(2,0);                          // sets cursor to column: 0 , row: 0
-    lcd.print(X);                                // print X value to the screen
-  }
+
+  lcd.clear();                                 // clear previous values from the screen
+  lcd.setCursor(0,0);                          // sets cursor to column: 0 , row: 0
+  lcd.print("X:");                             // print X: to the screen
+  lcd.setCursor(2,0);                          // sets cursor to column: 0 , row: 0
+  lcd.print(X);                                // print X value to the screen
+}
+
+void sendDataToESP() {
+  // === Read acceleromter data === //
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x32);                            // Start with register 0x32 (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(ADXL345, REG_SIZE, true);   // Read 6 registers total, each axis value is stored in 2 registers
+  X = ( Wire.read()| Wire.read() << 8);        // X-axis value
+  X = X/256;                                   //For a range of +-2g, we need to divide the raw values by 256, according to the datasheet
+  Y = ( Wire.read()| Wire.read() << 8);        // Y-axis value
+  Y = Y/256;
+  Z = ( Wire.read()| Wire.read() << 8);        // Z-axis value
+  Z = Z/256;
+  
+  Serial.print(X);
+  Serial.print("\t");
+  Serial.print(Y);
+  Serial.print("\t");
+  Serial.println(Z);
 }
 
 void lcdPrint(uint8_t incomingByte) {
   switch(incomingByte) {
   case '1':
-    switch(currentRow) {
-    case 1:
-      if (strlen(row1) < 9)
-        strcat(row1, "0");
-      break;
-    case 3:
-      if (strlen(row3) < 9)
-        strcat(row3, "0");
-      break;
-    default:
-      break;
-    }
+    rowFill(currentRow, "0");
     break;
   case '2':
-    switch(currentRow) {
-    case 1:
-      if (strlen(row1) < 9)
-        strcat(row1, "1");
-      break;
-    case 3:
-      if (strlen(row3) < 9)
-        strcat(row3, "1");
-      break;
-    default:
-      break;
-    }
+    rowFill(currentRow, "1");
     break;
   case '3':
-    switch(currentRow) {
-    case 1:
-      if (strlen(row1) < 9)
-        strcat(row1, "2");
-      break;
-    case 3:
-      if (strlen(row3) < 9)
-        strcat(row3, "2");
-      break;
-    default:
-      break;
-    }
+    rowFill(currentRow, "2");
     break;
   case '4':
-    switch(currentRow) {
-    case 1:
-      if (strlen(row1) < 9)
-        strcat(row1, "3");
-      break;
-    case 3:
-      if (strlen(row3) < 9)
-        strcat(row3, "3");
-      break;
-    default:
-      break;
-    }
+    rowFill(currentRow, "3");
     break;
   case '5':
-    switch(currentRow) {
-    case 1:
-      if (strlen(row1) < 9)
-        strcat(row1, "4");
-      break;
-    case 3:
-      if (strlen(row3) < 9)
-        strcat(row3, "4");
-      break;
-    default:
-      break;
-    }
+    rowFill(currentRow, "4");
     break;
   case '6':
-    switch(currentRow) {
-    case 1:
-      if (strlen(row1) < 9)
-        strcat(row1, "5");
-      break;
-    case 3:
-      if (strlen(row3) < 9)
-        strcat(row3, "5");
-      break;
-    default:
-      break;
-    }
+    rowFill(currentRow, "5");
     break;
   case '7':
     if (currentRow == 1) {
@@ -168,26 +107,16 @@ void lcdPrint(uint8_t incomingByte) {
     break;  
   case '9':
     if (currentRow == 1 || currentRow == 4) {
-      Serial.println("Clear screen");
       strcpy(row1, "");
       strcpy(row2, "");
       strcpy(row3, "");
       currentRow = 1;
-      Serial.println(atol(row1));
-      Serial.println(row2);
-      Serial.println(atol(row3));
-      Serial.println("Cleared screen");
     } else if (currentRow == 3) {
       if (strcmp(row2, "-")) {
         result = atol(row1) + atol(row3);
       } else {
         result = atol(row1) - atol(row3);
       }
-      Serial.println(atol(row1));
-      Serial.println(row2);
-      Serial.println(atol(row3));
-      Serial.print("Result is: "); 
-      Serial.println(String(result)); 
       currentRow = 4;    
     }    
     break;
@@ -203,6 +132,27 @@ void lcdPrint(uint8_t incomingByte) {
     Serial.println(atol(row1));
     Serial.println(row2);
     Serial.println(atol(row3));
+    break;
+  case 4:
+    Serial.println(atol(row1));
+    Serial.println(row2);
+    Serial.println(atol(row3));
+    Serial.print("Result is: "); 
+    Serial.println(String(result)); 
+  }
+}
+
+void rowFill(uint8_t row, char* ch) {
+  switch(row) {
+  case 1:
+    if (strlen(row1) < 9)
+      strcat(row1, ch);
+    break;
+  case 3:
+    if (strlen(row3) < 9)
+      strcat(row3, ch);
+    break;
+  default:
     break;
   }
 }
